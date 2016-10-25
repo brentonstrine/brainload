cdb0ee2b6245ab76aaab0c83698aa2617b2fc4bb
Brenton Strine <bstrine@rsglab.com>
2016-10-24 21:58:03 -0400

    thinking about history architecture

--- scripts/testCode.js
+++ scripts/testCode.js
@@ -2,11 +2,15 @@ define([], function() {
   return {
       testCodePrototype: (function(){
           var score = 0;
+          var history = [];
           this.updateScore= function(amount){
               score += amount;
           };
           this.getScore = function(){
               return score;
+          };
+          this.addToHistory= function(item){
+              history.push(item);
           };
           this.get = function(type){
               return "No component specified.";
