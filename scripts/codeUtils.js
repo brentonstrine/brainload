3aaa1b9c465e4cef29cbb6b21ef81a44cf9da9bc
Brenton Strine <bstrine@rsglab.com>
2016-10-24 21:57:05 -0400

    create code utils

---
+++ scripts/codeUtils.js
@@ -0,0 +1,51 @@
+define(["testCode", "components", "expressions"], function(testCode, components, expressions) {
+
+    var getProbabilities = function(codeList){
+        var l = codeList.length;
+        var inverseList = [];
+        var inverseTotal = 0;
+        var probabilityList = [];
+
+        for(i=0;i<l;i++){
+            var inverseScore = 1 / (codeList[i].getScore() + .1);
+            inverseList.push(inverseScore);
+            inverseTotal += inverseScore;
+        }
+        for(i=0;i<l;i++){
+            probabilityList.push(inverseList[i]/inverseTotal);
+        }
+        return probabilityList;
+    };
+
+    var chooseCode = function(codeList){
+        var probabilityList = getProbabilities(codeList);
+        var r = Math.random();
+        for(i=0;i<codeList.length;i++){
+            if(r < probabilityList[i]){
+                return i;
+            }
+            r = r - probabilityList[i];
+        }
+    };
+
+    var makeTest = function(codeList){
+        var probabilityList = getProbabilities(codeList);
+        var testString = "";
+        var errLocation;
+
+        var r = Math.random();
+        for(i=0;i<codeList.length;i++){
+            if(r < probabilityList[i]){
+            errLocation = i;
+                testString += codeList[i].get(false) + "\n";
+            } else {
+                testString += codeList[i].get(true) + "\n";
+            }
+            r -= probabilityList[i];
+        }
+        return [testString, errLocation];
+    };
+
+    return {makeTest};
+});
+
